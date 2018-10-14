079b7ceb3a2d205aea53e3ad9e6bfc0d0283e112
rdoran1 <33032112+rdoran1@users.noreply.github.com>
2018-10-15 00:42:34 +0100

    Commit 1
    First Commit
    Begginings of Game Development using elements from the Angry3D tutorial.

---
+++ Penalty Shooter Game/Assets/Scripts/BallDirection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDirection : MonoBehaviour {
+    
+    GameObject[] m_ballArray;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_ballArray = new GameObject[10];
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
